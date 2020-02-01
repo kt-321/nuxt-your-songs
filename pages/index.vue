bd5e3c3174ce3a888874ecbee085f46bba0cf06a
kt-321 <kt.shzk247@gmail.com>
2020-02-01 11:50:16 +0900

    first commit to develop

--- pages/index.vue
+++ pages/index.vue
@@ -3,27 +3,11 @@
     <div>
       <logo />
       <h1 class="title">
-        nuxt-your-songs
+        Your Songs
       </h1>
       <h2 class="subtitle">
-        My sublime Nuxt.js project
+        Welcome
       </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
     </div>
   </div>
 </template>
