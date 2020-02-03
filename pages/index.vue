4b98362274163dee99ed678e98e0ef835ee6bc95
kt-321 <kt.shzk247@gmail.com>
2020-02-03 23:39:26 +0900

    TypeScriptやESLintなどnpmパッケージインストール

--- pages/index.vue
+++ pages/index.vue
@@ -1,56 +1,53 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        Your Songs
-      </h1>
-      <h2 class="subtitle">
-        Welcome
-      </h2>
+    <div class="page-index">
+        <div>
+            <logo />
+            <h1 class="title">Your Songs</h1>
+            <h2 class="subtitle">Welcome</h2>
+        </div>
     </div>
-  </div>
 </template>
 
-<script>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
 import Logo from '~/components/Logo.vue'
 
-export default {
-  components: {
-    Logo
-  }
-}
+@Component({
+    components: {
+        Logo
+    }
+})
+export default class PageIndex extends Vue {}
 </script>
 
-<style>
+<style lang="stylus">
 .container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+    margin: 0 auto
+    min-height: 100vh
+    display: flex
+    justify-content: center
+    align-items: center
+    text-align: center
 }
 
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+    font-family: "Quicksand", "Source Sans Pro", -apple-system,
+        BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial,
+        sans-serif
+    display: block
+    font-weight: 300
+    font-size: 100px
+    color: #35495e
+    letter-spacing: 1px
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+.subtitle
+    font-weight: 300
+    font-size: 42px
+    color: #526488
+    word-spacing: 5px
+    padding-bottom: 15px
 
-.links {
-  padding-top: 15px;
-}
+.links
+    padding-top: 15px
 </style>
---
+++ prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    printWidth: 150,
+    tabWidth: 4,
+    useTabs: false,
+    semi: false,
+    singleQuote: true,
+    arrowParens: 'always',
+    jsxBracketSameLine: true
+}
